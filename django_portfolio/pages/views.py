373f6d3b6e79c889aedfea773e0daa07b41563e0
Salma Mokhtar Mohyadin <128319333+salmamokhtaar@users.noreply.github.com>
2025-09-04 15:06:05 +0300

    pages about ,contact

--- django_portfolio/pages/views.py
+++ django_portfolio/pages/views.py
@@ -3,3 +3,9 @@ from datetime import date
 
 def home(request):
     return render(request, "home.html", {"today": date.today()})
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
